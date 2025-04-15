91194b864458777920474790f4602aff10ec1e74
sakshi <sakshimathur5767@gmail.com>
2025-04-15 11:22:49 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Azure Kubernetes Service
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Grant AKS permission to pull from ACR
+resource "azurerm_role_assignment" "aks_acr" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group"
+  type        = string
+  default     = "rg-assesment"
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "East US2"
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name"
+  type        = string
+  default     = "sakshiacr25"
+}
+
+variable "aks_name" {
+  description = "Azure Kubernetes Service name"
+  type        = string
+  default     = "sakshiaks34"
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for AKS"
+  type        = string
+  default     = "demoaks"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the AKS cluster"
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "The VM size for the AKS node pool"
+  type        = string
+  default     = "Standard_DS2_v2"
+}
